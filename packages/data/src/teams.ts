efd9c596bbc1cf6a05eb0249b84b52c21b32aed5
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-04-07 16:12:13 +0300

    docs: getTeamById upgrade

--- packages/data/src/teams.ts
+++ packages/data/src/teams.ts
@@ -1,31 +1,36 @@
-import { apiCall } from "@dva/utils";
-import { isTeam, isTypedArray, ITeam } from "@dva/types";
+import { apiCall } from '@dva/utils';
+import { isTeam, isTypedArray, ITeam } from '@dva/types';
 
 let cachedAllTeamsList: Promise<ITeam[]>;
 export async function getAllTeams(): Promise<ITeam[]> {
-  if (typeof cachedAllTeamsList === "undefined")
-    cachedAllTeamsList = apiCall("teams").then((rawData: unknown) => {
-      if (isTypedArray(rawData, isTeam)) return rawData;
-      throw new Error(
-        `Unexpected value for teams array\n${JSON.stringify(rawData)}`
-      );
-    });
+    if (typeof cachedAllTeamsList === 'undefined')
+        cachedAllTeamsList = apiCall('teams').then((rawData: unknown) => {
+            if (isTypedArray(rawData, isTeam)) return rawData;
+            throw new Error(
+                `Unexpected value for teams array\n${JSON.stringify(rawData)}`,
+            );
+        });
 
-  return await cachedAllTeamsList;
+    return await cachedAllTeamsList;
 }
 
 const cachedTeamRecords: Record<string, Promise<ITeam>> = {};
 
+/**
+ * Get a {@link @dva/types#ITeam} by ID.
+ * @param ID of team to fetch.
+ * @returns
+ */
 export async function getTeamById(id: string): Promise<ITeam> {
-  let cached = cachedTeamRecords[id];
-  if (typeof cached === "undefined")
-    cached = cachedTeamRecords[id] = apiCall(`teams/${id}`).then(
-      (rawData: unknown) => {
-        if (isTeam(rawData)) return rawData;
-        throw new Error(
-          `Unexpected value for team\n${JSON.stringify(rawData)}`
+    let cached = cachedTeamRecords[id];
+    if (typeof cached === 'undefined')
+        cached = cachedTeamRecords[id] = apiCall(`teams/${id}`).then(
+            (rawData: unknown) => {
+                if (isTeam(rawData)) return rawData;
+                throw new Error(
+                    `Unexpected value for team\n${JSON.stringify(rawData)}`,
+                );
+            },
         );
-      }
-    );
-  return await cached;
+    return await cached;
 }
